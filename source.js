c05670ac036535dffe266689d7036d7a76efd14d
Tomer Einhorn <66746875+TomerEinhorn@users.noreply.github.com>
2020-11-15 20:08:05 +0200

    Create source.js

---
+++ source.js
@@ -0,0 +1,7 @@
+fetch('https://static.vidazoo.com/basev/vpt.js?userId=57b1cfb0f39a2d1100d8969c&playerId=5b228165ded76a0004ad5a55')
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (err) {
+        console.log("Something went wrong!", err);
+    });
