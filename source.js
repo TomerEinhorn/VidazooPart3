e7c27829168a73be02dd20433e7957bd030c83d9
Tomer Einhorn <66746875+TomerEinhorn@users.noreply.github.com>
2020-11-16 19:40:34 +0200

    Update source.js

--- source.js
+++ source.js
@@ -1,7 +1,12 @@
-fetch('https://static.vidazoo.com/basev/vpt.js?userId=57b1cfb0f39a2d1100d8969c&playerId=5b228165ded76a0004ad5a55')
-    .then(function (response) {
-        console.log(response);
-    })
-    .catch(function (err) {
-        console.log("Something went wrong!", err);
-    });
+function importExternal(url) {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script');
+    script.src = "https://static.vidazoo.com/basev/vpt.js?userId=57b1cfb0f39a2d1100d8969c&playerId=5b228165ded76a0004ad5a55";
+    script.async = true;
+    script.onload = () => resolve(window['external_global_component']);
+    script.onerror = reject;
+
+    document.body.appendChild(script);
+  });
+}
+
