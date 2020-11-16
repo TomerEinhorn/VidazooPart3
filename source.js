f6c8d6bd61c929ed4340e564b043572c02d6c58b
Tomer Einhorn <66746875+TomerEinhorn@users.noreply.github.com>
2020-11-16 19:52:11 +0200

    Update source.js

--- source.js
+++ source.js
@@ -1,7 +1,7 @@
 function importExternal(url) {
   return new Promise((resolve, reject) => {
     const script = document.createElement('script');
-    script.src = "https://static.vidazoo.com/basev/vpt.js?userId=57b1cfb0f39a2d1100d8969c&playerId=5b228165ded76a0004ad5a55";
+    script.src = url;
     script.async = true;
     script.onload = () => resolve(window['external_global_component']);
     script.onerror = reject;
