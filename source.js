9405b18eefe3aeff85e78385511d93f94a575e2d
Tomer Einhorn <66746875+TomerEinhorn@users.noreply.github.com>
2020-11-16 09:02:39 +0200

    Update source.js

--- source.js
+++ source.js
@@ -1,6 +1,6 @@
 fetch('https://static.vidazoo.com/basev/vpt.js?userId=57b1cfb0f39a2d1100d8969c&playerId=5b228165ded76a0004ad5a55')
     .then(function (response) {
-        return response.text();;
+        return response.text();
     })
     .catch(function (err) {
         console.log("Something went wrong!", err);
