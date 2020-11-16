b51f8e27579ea32f2d0354821c5b852e6b7ca7d5
Tomer Einhorn <66746875+TomerEinhorn@users.noreply.github.com>
2020-11-16 13:32:04 +0200

    Update source.js

--- source.js
+++ source.js
@@ -1,4 +1,7 @@
 fetch('https://static.vidazoo.com/basev/vpt.js?userId=57b1cfb0f39a2d1100d8969c&playerId=5b228165ded76a0004ad5a55')
-    .then(response => response.text())
-    .then(console.log(response);
-    .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (err) {
+        console.log("Something went wrong!", err);
+    });
